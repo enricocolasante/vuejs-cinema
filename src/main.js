bd5dae87b002954fd1d9dce17812b669643b5517
Enrico Colasante <ecolasante@vsware.ie>
2017-10-09 13:16:06 +0100

    Vue.js cinema

---
+++ src/main.js
@@ -0,0 +1,51 @@
+import Vue from 'vue';
+import './style.scss';
+
+import genres from './util/genres';
+
+import Overview from './components/Overview.vue';
+
+import routes from './util/routes';
+
+import VueResource from 'vue-resource';
+Vue.use(VueResource);
+
+import moment from 'moment-timezone';
+moment.tz.setDefault("UTC");
+Object.defineProperty(Vue.prototype, '$moment', { get() {return this.$root.moment}});
+
+import { checkFilter, setDay } from './util/bus';
+
+const bus = new Vue();
+Object.defineProperty(Vue.prototype, '$bus', { get() {return this.$root.bus}});
+
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
+
+const router = new VueRouter({ routes });
+
+import Tooltip from './util/tooltip';
+Vue.use(Tooltip);
+
+new Vue({
+    el: '#app',
+    data: {
+        genre: [],
+        time: [],
+        movies: [],
+        moment,
+        day:moment(),
+        bus
+    },
+    components: {
+        Overview
+    },
+    created() {
+        this.$http.get('/api').then(response => {
+            this.movies = response.data;
+        });
+        this.$bus.$on('check-filter', checkFilter.bind(this));
+        this.$bus.$on('set-day', setDay.bind(this));
+    },
+    router
+});
